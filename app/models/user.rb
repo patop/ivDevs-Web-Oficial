3cc7ec6cbd78ef749410f597b0ce81685420658b
fbarriosCL <fbarrios.ing.informatico@gmail.com>
2016-04-20 01:32:34 -0300

    ADD configurate views #3

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+  def full_name
+  	name.humanize.to_s + ' ' + lastname.humanize.to_s
+  end
 end
