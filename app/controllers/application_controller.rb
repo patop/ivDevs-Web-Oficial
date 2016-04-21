026eb8f5255fdb20ce451a9f7475fa782ce9a380
fbarriosCL <fbarrios.ing.informatico@gmail.com>
2016-04-20 21:58:07 -0300

    FIX add attributes for views edit #2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-  	devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :name,:lastname,:email, :password, :password_confirmation) }
-  	devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :name,:lastname,:email, :password, :password_confirmation) }
+  	devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :name,:lastname,:email, :password, :password_confirmation, :facebook, :twitter, :linkedin, :role, :aboutme) }
+  	devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :name,:lastname,:email, :password, :password_confirmation, :facebook, :twitter, :linkedin, :role, :aboutme) }
   end
  end
