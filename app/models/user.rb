d0fd35c881d65525d306e375fd175d7dc97031ad
fbarriosCL <fbarrios.ing.informatico@gmail.com>
2016-04-20 21:42:57 -0300

    ADD attributes for user and roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-
+  enum role: [:admin, :editor, :miembro]
+  
   def full_name
   	name.humanize.to_s + ' ' + lastname.humanize.to_s
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160420034610) do
+ActiveRecord::Schema.define(version: 20160421003626) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -31,6 +31,11 @@ ActiveRecord::Schema.define(version: 20160420034610) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "facebook"
+    t.string   "twitter"
+    t.string   "linkedin"
+    t.integer  "role"
+    t.text     "aboutme"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
