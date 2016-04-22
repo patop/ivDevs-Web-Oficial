af729a99cf0b1256bbb9991634c2b920def1bb68
rocket4ce <rocket4ce@gmail.com>
2016-04-22 00:41:14 -0300

    ADD current user create events  #14

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -15,7 +15,7 @@ class EventsController < ApplicationController
 
   # GET /events/new
   def new
-    @event = Event.new
+    @event = current_user.events.new
   end
 
   # GET /events/1/edit
@@ -25,7 +25,7 @@ class EventsController < ApplicationController
   # POST /events
   # POST /events.json
   def create
-    @event = Event.new(event_params)
+    @event = current_user.events.new(event_params)
 
     respond_to do |format|
       if @event.save
