82d3c94d12b885bdc2ab2951dc2590aa936b96b6
rocket4ce <rocket4ce@gmail.com>
2016-04-22 01:52:48 -0300

    ADD geocoder logic on events model #14

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,12 @@
 class Event < ActiveRecord::Base
+  #geocoder
+  geocoded_by :address   # can also be an IP address
+	after_validation :geocode          # auto-fetch coordinates
+  # asociaciones
   belongs_to :tag
   belongs_to :user
+
+
 
   enum type_event: [:meetup, :workshop, :evento]
 end
