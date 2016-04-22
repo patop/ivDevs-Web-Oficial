afba84d72f87c0e19d3bb9922936d573d6fdc604
rocket4ce <rocket4ce@gmail.com>
2016-04-22 00:34:04 -0300

    ADD agregue tag references  #14

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160422033113) do
+ActiveRecord::Schema.define(version: 20160422033336) do
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
@@ -26,8 +26,10 @@ ActiveRecord::Schema.define(version: 20160422033113) do
     t.integer  "user_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "tag_id"
   end
 
+  add_index "events", ["tag_id"], name: "index_events_on_tag_id"
   add_index "events", ["user_id"], name: "index_events_on_user_id"
 
   create_table "users", force: :cascade do |t|
